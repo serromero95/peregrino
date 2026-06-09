5be50fb46393b1e60ec0092bfc042ae64aee55f2
Sergio Romero <sergioromeroboiro@gmail.com>
2026-06-09 18:45:06 +0200

    Use class for player element

--- js/game.js
+++ js/game.js
@@ -1,1 +1,7 @@
+const gameContainer = document.getElementById("game-container");
 
+const player = document.createElement("div");
+
+player.className = "player";
+
+gameContainer.appendChild(player);
