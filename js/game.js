640fc4a09e31caac4e6eb19939224b4d34001e82
Sergio Romero <sergioromeroboiro@gmail.com>
2026-06-11 01:09:43 +0200

    Add player movement

--- js/game.js
+++ js/game.js
@@ -11,3 +11,19 @@ const ground = document.createElement("div");
 ground.className = "ground";
 
 gameContainer.appendChild(ground);
+
+let playerX = 80;
+
+document.addEventListener("keydown", function(event) {
+    if(event.key === "ArrowRight"){
+        playerX = playerX + 10;
+    }
+    player.style.left = playerX + "px";
+});
+
+document.addEventListener("keydown", function(event) {
+    if(event.key === "ArrowLeft"){
+        playerX = playerX - 10;
+    }
+    player.style.left = playerX + "px";
+});
