a8caa7fad7e969f920e1c14836f69a5f0930a6cc
Sergio Romero <sergioromeroboiro@gmail.com>
2026-06-16 00:39:35 +0200

    Add player jump and continuous movement

--- js/game.js
+++ js/game.js
@@ -14,24 +14,57 @@ gameContainer.appendChild(ground);
 
 let playerX = 80;
 
+let playerY = 60;
+
+let velocityY = 0;
+let gravity = 1;
+let isJumping = false;
+let jumpForce = 18;
+
+let movingRight = false;
+let movingLeft = false;
+let playerSpeed = 0;
+
 document.addEventListener("keydown", function(event) {
     if(event.key === "ArrowRight"){
-        playerX = playerX + 10;
-        player.style.left = playerX + "px";
+        movingRight = true;
+        playerSpeed = 10;
     }
 
     if(event.key === "ArrowLeft"){
         playerX = playerX - 10;
         player.style.left = playerX + "px";
     }
+
+    if(event.code === "Space" && !isJumping) {
+        isJumping = true;
+        velocityY = jumpForce;
+    }
+
 });
 
-let playerY = 60;
+document.addEventListener("keyup", function(event) {
+    if(event.key === "ArrowRight"){
+        movingRight = false;
+    }
+});
 
 setInterval(function() {
-    playerY = playerY - 5;
-    player.style.bottom = playerY + "px";
+
+    if(movingRight){
+        playerX = playerX + playerSpeed;
+        player.style.left = playerX + "px";
+    }
+
+    playerY = playerY + velocityY;
+    velocityY = velocityY - gravity;
+
     if(playerY < 60) {
         playerY = 60;
+        velocityY = 0;
+        isJumping = false;
     }
+
+    player.style.bottom = playerY + "px";
+
 }, 16);
